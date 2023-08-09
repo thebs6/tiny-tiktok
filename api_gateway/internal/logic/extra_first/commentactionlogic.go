e90e234d3ce46e2fa1c663ed664d87fb273b009b
gaochengxi <1825752554@qq.com>
2023-08-09 10:08:08 +0800

    tmp

--- api_gateway/internal/logic/extra_first/commentactionlogic.go
+++ api_gateway/internal/logic/extra_first/commentactionlogic.go
@@ -2,11 +2,15 @@ package extra_first
 
 import (
 	"context"
+	"encoding/json"
 
 	"tiny-tiktok/api_gateway/internal/svc"
 	"tiny-tiktok/api_gateway/internal/types"
+	"tiny-tiktok/service/comment/pb/comment"
 
+	"github.com/zeromicro/go-zero/core/discov"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type CommentActionLogic struct {
@@ -24,7 +28,27 @@ func NewCommentActionLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Com
 }
 
 func (l *CommentActionLogic) CommentAction(req *types.CommentActionReq) (resp *types.CommentActionResp, err error) {
-	// todo: add your logic here and delete this line
-
+	conn := zrpc.MustNewClient(zrpc.RpcClientConf{
+		Etcd: discov.EtcdConf{
+			Hosts: []string{"127.0.0.1:2379"},
+			Key:   "user.rpc",
+		},
+	})
+	client := comment.NewCommentServiceClient(conn.Conn())
+	client.CommentAction(l.ctx, &comment.CommentActionReq{
+		UserId: req.,
+	})
 	return
+}
+
+func GetUidFromCtx(ctx context.Context) int64 {
+	var uid int64
+	if jsonUid, ok := ctx.Value(CtxKeyJwtUserId).(json.Number); ok {
+		if int64Uid, err := jsonUid.Int64(); err == nil {
+			uid = int64Uid
+		} else {
+			logx.WithContext(ctx).Errorf("GetUidFromCtx err : %+v", err)
+		}
+	}
+	return uid
 }
