e4bbdc6881b339deb4f44601e3e0860a8d4aa9c1
gaochengxi <1825752554@qq.com>
2023-08-11 20:19:43 +0800

    Add: comment action_type=1  redis cache

---
+++ service/comment/internal/redismodel/commentmodel.go
@@ -0,0 +1,52 @@
+package redismodel
+
+import (
+	"context"
+	"strconv"
+	"tiny-tiktok/service/comment/internal/model"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// format: comment-video-{video_id}
+
+type CommentModel struct {
+	redcli    *redis.Client
+	table     string
+	keyPrefix string
+}
+
+func NewCommentModel(redcli *redis.Client) CommentModel {
+	return CommentModel{
+		redcli:    redcli,
+		table:     "`comment`",
+		keyPrefix: "comment-video-",
+	}
+}
+
+func (m *CommentModel) Exists(ctx context.Context, video_id int64) int64 {
+	key := m.keyPrefix + strconv.FormatInt(video_id, 10)
+	return m.redcli.Exists(ctx, key).Val()
+}
+
+func (m *CommentModel) ZAdd(ctx context.Context, video_id int64, score int64, comment *model.Comment) error {
+	key := m.keyPrefix + strconv.FormatInt(video_id, 10)
+
+	// Question: why use comment's pointer as the Member?
+	_, err := m.redcli.ZAdd(ctx, key, redis.Z{
+		Score:  float64(score),
+		Member: comment,
+	}).Result()
+	return err
+}
+
+func (m *CommentModel) ZRevRangeWithScores(ctx context.Context, video_id int64) ([]redis.Z, error) {
+	key := m.keyPrefix + strconv.FormatInt(video_id, 10)
+	resList, err := m.redcli.ZRevRangeWithScores(ctx, key, 0, -1).Result()
+	return resList, err
+}
+
+func (m *CommentModel) Del(ctx context.Context, video_id int64) {
+	key := m.keyPrefix + strconv.FormatInt(video_id, 10)
+	m.redcli.Del(ctx, key)
+}
